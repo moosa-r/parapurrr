65ed9526d56e4fafd6b012d4d3c807bf75db5907
Moosa Rezwani <moosa.rezwani@gmail.com>
2021-01-14 21:40:48 +0330

    Fix test issuein R CMD Check

--- tests/testthat/test-imap_family.R
+++ tests/testthat/test-imap_family.R
@@ -1,37 +1,39 @@
+n_cores = 2
+
 x = runif(5, min = 1, 100)
 names(x) = runif(5, min = 1, 100)
 
 test_that("pa_imap works", {
   expect_identical(purrr::imap(x, ~paste0(.x,.y)),
-                   parapurrr::pa_imap(x, ~paste0(.x,.y), .verbose = TRUE))
+                   parapurrr::pa_imap(x, ~paste0(.x,.y), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_lgl works", {
   expect_identical(purrr::imap_lgl(x, ~(.x > as.numeric(.y))),
-                   parapurrr::pa_imap_lgl(x, ~(.x > as.numeric(.y)), .verbose = TRUE))
+                   parapurrr::pa_imap_lgl(x, ~(.x > as.numeric(.y)), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_int works", {
   expect_identical(purrr::imap_int(x, ~as.integer(.x * as.numeric(.y))),
-                   parapurrr::pa_imap_int(x, ~as.integer(.x * as.numeric(.y)), .verbose = TRUE))
+                   parapurrr::pa_imap_int(x, ~as.integer(.x * as.numeric(.y)), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_dbl works", {
   expect_identical(purrr::imap_dbl(x, ~.x * as.numeric(.y)),
-                   parapurrr::pa_imap_dbl(x, ~.x * as.numeric(.y), .verbose = TRUE))
+                   parapurrr::pa_imap_dbl(x, ~.x * as.numeric(.y), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_chr works", {
   expect_identical(purrr::imap_chr(x, ~.x * as.numeric(.y)),
-                   parapurrr::pa_imap_chr(x, ~.x * as.numeric(.y), .verbose = TRUE))
+                   parapurrr::pa_imap_chr(x, ~.x * as.numeric(.y), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_dfr works", {
   expect_identical(purrr::imap_dfr(x, ~.x * as.numeric(.y)),
-                   parapurrr::pa_imap_dfr(x, ~.x * as.numeric(.y), .verbose = TRUE))
+                   parapurrr::pa_imap_dfr(x, ~.x * as.numeric(.y), cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_imap_dfc works", {
   expect_identical(purrr::imap_dfc(x, ~.x * as.numeric(.y)),
-                   parapurrr::pa_imap_dfc(x, ~.x * as.numeric(.y), .verbose = TRUE))
+                   parapurrr::pa_imap_dfc(x, ~.x * as.numeric(.y), cores = n_cores, .verbose = TRUE))
 })
--- tests/testthat/test-map2_family.R
+++ tests/testthat/test-map2_family.R
@@ -1,43 +1,45 @@
+n_cores = 2
+
 x = as.list(as.integer(1:5))
 y = as.list(as.integer(6:10))
 names(x) = paste0("element_", 1:5)
 
 test_that("pa_map2 works", {
   expect_identical(purrr::map2(x, y, ~.x * .y),
-                   parapurrr::pa_map2(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_lgl works", {
   expect_identical(purrr::map2_lgl(x, y, ~.x + .y > 9),
-                   parapurrr::pa_map2_lgl(x, y, ~.x + .y > 9, .verbose = TRUE))
+                   parapurrr::pa_map2_lgl(x, y, ~.x + .y > 9, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_int works", {
   expect_identical(purrr::map2_int(x, y, ~.x * .y),
-                   parapurrr::pa_map2_int(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2_int(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_dbl works", {
   expect_identical(purrr::map2_dbl(x, y, ~.x * .y * 2.3),
-                   parapurrr::pa_map2_dbl(x, y, ~.x * .y * 2.3, .verbose = TRUE))
+                   parapurrr::pa_map2_dbl(x, y, ~.x * .y * 2.3, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_chr works", {
   expect_identical(purrr::map2_chr(x, y, ~.x * .y),
-                   parapurrr::pa_map2_chr(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2_chr(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_df works", {
   expect_identical(purrr::map2_df(x, y, ~.x * .y),
-                   parapurrr::pa_map2_df(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2_df(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_dfr works", {
   expect_identical(purrr::map2_dfr(x, y, ~.x * .y),
-                   parapurrr::pa_map2_dfr(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2_dfr(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
 
 test_that("pa_map2_dfc works", {
   expect_identical(purrr::map2_dfc(x, y, ~.x * .y),
-                   parapurrr::pa_map2_dfc(x, y, ~.x * .y, .verbose = TRUE))
+                   parapurrr::pa_map2_dfc(x, y, ~.x * .y, cores = n_cores, .verbose = TRUE))
 })
