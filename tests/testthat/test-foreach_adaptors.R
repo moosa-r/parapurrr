5975197e9d0b1d1b3f88c6b1c9770ca9f4aae32c
Moosa Rezwani <moosa.rezwani@gmail.com>
2021-01-17 06:33:00 +0330

    remove tests that need external packages

--- tests/testthat/test-foreach_adaptors.R
+++ tests/testthat/test-foreach_adaptors.R
@@ -12,18 +12,9 @@ test_that("doParallel works on Unix", {
   expect_error(parapurrr::pa_map(x, sqrt, cores = n_cores, adaptor = "doParallel", cluster_type = "FORK"), NA)
 })
 
-test_that("doSNOW works on windows", {
+test_that("doSNOW works", {
   skip_if_not_installed("doSNOW")
   expect_error(parapurrr::pa_map(x, sqrt, cores = n_cores, adaptor = "doSNOW", cluster_type = "SOCK"), NA)
-})
-
-test_that("doSNOW works on Unix", {
-  skip_if_not_installed("doSNOW")
-  skip_on_os("windows")
-  skip_if_not_installed("Rmpi")
-  expect_error(parapurrr::pa_map(x, sqrt, cores = n_cores, adaptor = "doSNOW"), cluster_type = "MPI")
-  skip_if_not_installed("nws")
-  expect_error(parapurrr::pa_map(x, sqrt, cores = n_cores, adaptor = "doSNOW"), cluster_type = "NWS")
 })
 
 test_that("doFuture works on windows", {
