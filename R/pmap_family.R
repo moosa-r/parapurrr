ab0dde9f94ef0c5cb3693781f99af84604782f7f
Moosa Rezwani <moosa.rezwani@gmail.com>
2021-05-07 03:23:00 +0430

    Implement purrr pmap family

--- R/pmap_family.R
+++ R/pmap_family.R
@@ -30,3 +30,384 @@
     }
   }
 }
+
+#' @rdname pa_map2
+#' @export
+pa_pmap <- function(.l, .f,
+                    cores = NULL,
+                    adaptor = "doParallel", cluster_type = NULL,
+                    splitter = NULL, auto_export = TRUE, .export = NULL,
+                    .packages = NULL, .noexport = NULL,
+                    .errorhandling = "stop",
+                    .inorder = TRUE,
+                    .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_lgl <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap_lgl(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_int <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap_int(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_dbl <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap_dbl(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_chr <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap_chr(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_df <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("`pmap_df()` requires dplyr.")
+  }
+
+  int_fun <- quote(purrr::pmap(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = dplyr::bind_rows,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_dfr <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("`pmap_dfr()` requires dplyr.")
+  }
+
+  int_fun <- quote(purrr::pmap(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = dplyr::bind_rows,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pmap_dfc <- function(.l, .f,
+                        cores = NULL,
+                        adaptor = "doParallel", cluster_type = NULL,
+                        splitter = NULL, auto_export = TRUE, .export = NULL,
+                        .packages = NULL, .noexport = NULL,
+                        .errorhandling = "stop",
+                        .inorder = TRUE,
+                        .verbose = FALSE) {
+
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("`pmap_dfr()` requires dplyr.")
+  }
+
+  int_fun <- quote(purrr::pmap(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = dplyr::bind_cols,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  return(output)
+}
+
+#' @rdname pa_map2
+#' @export
+pa_pwalk <- function(.l, .f,
+                    cores = NULL,
+                    adaptor = "doParallel", cluster_type = NULL,
+                    splitter = NULL, auto_export = TRUE, .export = NULL,
+                    .packages = NULL, .noexport = NULL,
+                    .errorhandling = "stop",
+                    .inorder = TRUE,
+                    .verbose = FALSE) {
+
+  int_fun <- quote(purrr::pmap(x, .f))
+
+  if (is.data.frame(.l)) {
+    .l <- as.list(.l)
+  }
+
+  output <- .pa_internal(.x = NULL,
+                         .y = NULL,
+                         .l = .l,
+                         .f = .f,
+                         int_fun = int_fun,
+                         adaptor = adaptor,
+                         cores = cores,
+                         cluster_type = cluster_type,
+                         auto_export = auto_export,
+                         splitter = splitter,
+                         .combine = c,
+                         .init = NULL,
+                         .final = NULL,
+                         .inorder = .inorder,
+                         .multicombine = TRUE,
+                         .maxcombine = NULL,
+                         .errorhandling = .errorhandling,
+                         .packages = .packages,
+                         .export = .export,
+                         .noexport = .noexport,
+                         .verbose = .verbose)
+  invisible(.l)
+}
