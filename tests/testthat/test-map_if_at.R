3ad5d708736066d62c5fca255edea4b4bdfb3ef5
Moosa Rezwani <moosa.rezwani@gmail.com>
2021-05-06 07:36:12 +0430

    add test for pa_map_if & pa_map_at

---
+++ tests/testthat/test-map_if_at.R
@@ -0,0 +1,21 @@
+n_cores <- 2
+
+x <- as.list(as.integer(1:5))
+names(x) <- paste0("element_", 1:5)
+
+test_that("pa_map_if works", {
+  expect_identical(purrr::map_if(.x = x, .p = ~.x%%2==0, .f = ~"is.even"),
+                   parapurrr::pa_map_if(.x = x, .p = ~.x%%2==0, .f = ~"is.even",
+                                        cores = n_cores, .verbose = TRUE))
+  expect_identical(purrr::map_if(.x = x, .p = ~.x%%2==0,
+                                 .f = ~"is.even", .else = ~"is.false"),
+                   parapurrr::pa_map_if(.x = x, .p = ~.x%%2==0,
+                                        .f = ~"is.even", .else = ~"is.false",
+                                        cores = n_cores, .verbose = TRUE))
+})
+
+test_that("pa_map_at works", {
+  expect_identical(purrr::map_at(.x = x, .at = c(1,4), .f = ~.x * 1000),
+                   parapurrr::pa_map_at(.x = x, .at = c(1,4), .f = ~.x * 1000,
+                                        cores = n_cores, .verbose = TRUE))
+})
